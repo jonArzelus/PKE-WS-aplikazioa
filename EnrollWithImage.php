72d37451c13dfb4e447eefd2c2e6ef6d337d8e83
jonArzelus <kontrabass32@hotmail.com>
2016-10-10 21:06:47 +0200

    zerbitzari aldeko konprobaketa guztiak eginak, regexp erabiliz

--- EnrollWithImage.php
+++ EnrollWithImage.php
@@ -36,23 +36,30 @@
 		if (mysqli_num_rows($emaitza) > 0) {
 			echo "Dagoeneko existitzen da $eposta duen erabiltzaile bat. Mesedez, atzera eman eta sartu ezazu beste posta-elektroniko helbide bat... </br></br>";
 		} else { //ez da erabiltzailerik aurkitu email horrekin
+
+			//ikusi ea REGEXP pasatzen duen
+			$esp_izena= filter_var($izena, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/([A-Z]{1}[a-z ]{1,})*/")));
+			$esp_mota = filter_var($mota, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^(GUEST|USER|ADMIN)$/")));
+			$esp_eposta= filter_var($eposta, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z]+[0-9]{3}@ikasle.ehu.(eus|es)$/")));
+			$esp_pass= filter_var($pass, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/.{6,}$/")));
+			$esp_tel= filter_var($tel, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/[0-9]{9}/")));
 		
-			if(!empty($_FILES['argazki-fitxategia']['name'])) {
-				//argazkia /irudiak karpetan gordetzen
-				$izenaPosta = $eposta;
-				$izenaPosta = str_replace(".", "", $izenaPosta);
-				$izenaPosta = str_replace("@", "", $izenaPosta);
-				$argazkia_izena = $_FILES["argazki-fitxategia"]["name"];
-				$argazkia_ruta = $_FILES["argazki-fitxategia"]["tmp_name"];
-				$argazkia_helbidea = "erabiltzaileIrudiak/".$izenaPosta; //erabiltzaile argazkiei izen berri bat ezarri beren emailaren arabera
-				copy($argazkia_ruta,$argazkia_helbidea);
-			} else {
-				$argazkia_helbidea = "ARGAZKI_GABE";
-				$argazkia_izena = null;
-			}
-			
-			if(!filter_var($eposta, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z]+[0-9]{3}@ikasle.ehu.(eus|es)$/")))===false) {
-			
+			if((!$esp_izena==false) && (!$esp_mota==false) && (!$esp_eposta==false) && (!$esp_pass==false) && (!$esp_tel==false)) {
+
+				if(!empty($_FILES['argazki-fitxategia']['name'])) {
+					//argazkia /irudiak karpetan gordetzen
+					$izenaPosta = $eposta;
+					$izenaPosta = str_replace(".", "", $izenaPosta);
+					$izenaPosta = str_replace("@", "", $izenaPosta);
+					$argazkia_izena = $_FILES["argazki-fitxategia"]["name"];
+					$argazkia_ruta = $_FILES["argazki-fitxategia"]["tmp_name"];
+					$argazkia_helbidea = "erabiltzaileIrudiak/".$izenaPosta; //erabiltzaile argazkiei izen berri bat ezarri beren emailaren arabera
+					copy($argazkia_ruta,$argazkia_helbidea);
+				} else {
+					$argazkia_helbidea = "ARGAZKI_GABE";
+					$argazkia_izena = null;
+				}
+
 				echo "Kaixo $izena, hurrengo datuak gordeko dira datubasean:</br></br>
 				Izena: $izena </br>
 				Eposta: $eposta</br>
@@ -75,7 +82,18 @@
 				//tauletan datuak gordetzea
 				mysqli_query($esteka,"INSERT INTO erabiltzaileak (IzenAbizena, erabiltzaileMota, PostaElektronikoa, Pasahitza, TelefonoZenbakia, Espezialitatea, Interesak, Argazkia) VALUES ('$izena', '$mota', '$eposta', '$pass', '$tel', '$esp', '$interesak', '$argazkia_helbidea')");
 			} else {
-				echo "Idazikako eposta helbidea ez da zuzena! </br>";
+				echo "Datuak jasotzean errorea(k):</br>";
+				if($esp_izena==false)
+					echo "- Izena letra larriz hasi behar da eta ondoren letra xehez jarraitu. </br>"; 
+				if($esp_mota==false)
+					echo "- Erabiltzaile mota okerra. Kontaktatu administratzailea.</br>";
+				if($esp_eposta==false)
+					echo "- Posta-elektronikoa izena000@ikasle.ehu.eus motakoa izan behar da (unibertsitatekoa).</br>";
+				if($esp_pass==false)
+					echo "Pasahitzak gutxienez 6 letrako luzeera izan behar du.</br>";
+				if($esp_tel==false)
+					echo "- Telefono zenbakiak soilik 9 digitu izan ditzazte.</br>";
+				echo "Jasotako datuak ez dira zuzenak. Mesedez, saiatu berriro beranduago... </br></br>";
 			}
 		}
 		echo "Hauek dira ".$_SERVER['REQUEST_METHOD']." metodoko datuak:</br>";
