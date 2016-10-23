7446301310aca2d1e80988c37c0bb3b3f0f90a7d
Inaki Berriotxoa ubuntu <berriotxoa86@gmail.com>
2016-10-23 17:42:05 +0200

    1 eta 2 hautazkoak ya-ya bukatuta

--- getUserInform.php
+++ getUserInform.php
@@ -24,6 +24,34 @@
 		   type='text/css' 
 		   media='only screen and (max-width: 480px)'
 		   href='stylesPWS/smartphone.css' />
+	<script type="text/javascript">
+		function begiratu(){
+			
+			var dago=false;
+			var xmlDoc = document.getElementById('datuak').contentDocument;
+			
+			var kor = document.getElementById("emaila").value;
+			var epostenZer=xmlDoc.getElementsByTagName("eposta");
+			var tfnoZer=xmlDoc.getElementsByTagName("telefonoa");
+			var izenZer=xmlDoc.getElementsByTagName("izena");
+			var AbizenZer=xmlDoc.getElementsByTagName("abizena1");
+			var Abizen2Zer=xmlDoc.getElementsByTagName("abizena2");
+			
+			
+			for (var i = 0; i < epostenZer.length; i++) {
+				if (kor==epostenZer[i].childNodes[0].nodeValue){
+					document.getElementById('tfonoa').value=tfnoZer[i].childNodes[0].nodeValue;
+					document.getElementById('izena').value=izenZer[i].childNodes[0].nodeValue +"  " + AbizenZer[i].childNodes[0].nodeValue + "  " + Abizen2Zer[i].childNodes[0].nodeValue ;
+					dago=true;
+					break;
+				}
+			}
+			if(!dago){
+				alert("Eposta hau ez dago UPV/EHUn erregistraturik. Sar ezazu beste bat");
+			}
+
+		}
+	</script>
   </head>
   <body>
   <div id='page-wrap'>
@@ -44,15 +72,16 @@
     ?>
       
 	<h2>Quiz: crazy questions</h2>
+	<OBJECT id="datuak" data="xml/erabiltzaileak.xml" type="text/xml" style="display:none"></OBJECT>
     </header>
 	<nav class='main' id='n1' role='navigation'>
-		<a href='layout.php'><span class="act-sel">Hasiera</span></a>
+		<a href='layout.php'><span >Hasiera</span></a>
 		<a href='Quizzes.php'><span>Galderak</span></a>
 	<?php
 	if($_SESSION['eposta'] != "guest")
 		echo'<a href="InsertQuestion.php"><span>Galdera Sortu</span></a>';
 	if($_SESSION['eposta'] == "rosa123@ikasle.ehu.eus")
-		echo'<a href="getUserInform.php"><span>Ikaslea begiratu</span></a>';
+		echo'<a href="getUserInform.php"><span class="act-sel">Ikaslea begiratu</span></a>';
 	?>
 		<a href='credits.php'><span>Kredituak</span></a>
 	</nav>
@@ -60,25 +89,20 @@
 		
 	<h1>Sartu ikaslearen e-maila</h1> <br/>
 	<div>
-		<form id="ikasle_emaila" name="ikasle_emaila" method="POST" action="getUserInform.php" enctype="multipart/form-data">
-			<div class="begiratu_ikaslea">
+		<form id="ikasle_emaila" name="ikasle_emaila">
+			
   				<h3>Posta-elektronikoa:</h3> 
-  				<input type="text" name="eposta" title="Sartu zure emaila." placeholder="E-mail"><br><br>
+  				<input type="text" id="emaila" name="emaila" required  title="Sartu ikasle emaila" placeholder="E-mail" oninvalid="this.setCustomValidity('Sartu ikasle baten e-posta')"><br><br>
 				<h3>Ikaslearen izena:</h3>
- 				<input type="text" name="ikasle_izena" title="Hau da ikaslearen izena." placeholder="Ikasle izena"><br><br>
+ 				<input type="text" id="izena" name="ikasle_izena" title="Hau da ikaslearen izena." placeholder="Ikasle izena"><br><br>
 				<h3>Telefono zenbakia:</h3>
- 				<input type="text" name="telefono_zenbakia" title="Hau da ikaslearen telefono zenbakia." placeholder="Telfono zenbakia"><br><br>
+ 				<input type="text" id="tfonoa" name="telefono_zenbakia" title="Hau da ikaslearen telefono zenbakia." placeholder="Telfono zenbakia"><br><br>
 				<br><br>
-				<input type="submit" class="ikasle-botoia" name="button" value="Begiratu">
-				
-			</div>
+				<input type="submit" class="ikasle-botoia" name="button" value="Begiratu" onClick="begiratu()">
+			
 		</form>
 	</div>
-		<?php 
 		
-		
-		
-		?>
 		
     </section>
 	<footer class='main' id='f1'>
