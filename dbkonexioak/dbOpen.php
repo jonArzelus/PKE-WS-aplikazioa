61bbeb409b645f29229cb030d35469733c69cdd5
jarzelus001 <jarzelus001@ikasle.ehu.es>
2016-11-03 09:49:48 +0100

    konexioa ondo egitean mezua kendua

--- dbkonexioak/dbOpen.php
+++ dbkonexioak/dbOpen.php
@@ -5,7 +5,8 @@ $db = new mysqli(HOST, USER, PASS, DATABASE);
 	if($db->connect_error){
 		die ("<div class='message'>Datu basera konektatzean akatsa egon da: (".$db->connect_errno.")".$db->connect_error."</div></br>");
 	}else{
-		echo("<div class='message'>Datu basera konexioa ongi burutu da: ".$db->host_info."</div></br>");
+		//echo("<div class='message'>Datu basera konexioa ongi burutu da: ".$db->host_info."</div></br>");
+		null;
 	}
 
 ?>
