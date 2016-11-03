c6a178538f98c8b733c756329dae7251cc1f26ee
Inaki Berriotxoa ubuntu <berriotxoa86@gmail.com>
2016-11-03 16:36:40 +0100

    derrigorrezko bigarrena bukatzeke

--- php/egiaztatuPasahitza.php
+++ php/egiaztatuPasahitza.php
@@ -2,23 +2,32 @@
 <?php
 
 //nusoap.php klasea gehitzen dugu
-require_once('../lib/nusoap.php');
-require_once('../lib/class.wsdlcache.php');
-//soap server motako objektua sortzen dugu
+require_once('lib/nusoap.php');
+require_once('lib/class.wsdlcache.php');
+
+$URL       = "localhost";
+$namespace = $URL.'?wsdl';
+
 //soap_server motako objektua sortzen dugu
 $server = new soap_server;
+$server->configureWSDL('hellotesting', $namespace);
 
-//inplementatu nahi dugun funtzioa erregistratzen dugu
-//funtzio bat baino gehiago erregistra liteke ...
+//inplementatu nahi dugun funtzioa erregistratzen dugu, funtzio bat baino gehiago erregistra liteke ...
 $server->register('pasahitzaKonprobatu');
 
 //funtzioa inplementatzen da
 function pasahitzaKonprobatu($eposta){
 	//hemen konprobatu behar da pasahitza fitxategian dagoen. kontuz fgets funtzioaren zuriuneekin
-	return $x + $y;
+	//for(){
+		$myfile = fopen("toppaswords.txt", "r")or die("Unable to open file!");
+		echo fread($myfile,filesize("toppaswords.txt"));
+		fclose($myfile);
+		return 1;
+	//}
 }
-}
+
 //nusoap klaseko service metodoari dei egiten diogu
 $HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : '';
 $server->service($HTTP_RAW_POST_DATA);
+exit();
 ?>
