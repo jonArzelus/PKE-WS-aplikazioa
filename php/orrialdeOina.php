a84227a4cd6dbab0381762eff7b2f37caf465e1d
jarzelus001 <jarzelus001@ikasle.ehu.es>
2016-11-03 16:25:57 +0100

    PKEko diseinu aldaketak

--- php/orrialdeOina.php
+++ php/orrialdeOina.php
@@ -1,6 +1,7 @@
 	<footer class='main' id='f1'>
-		<p><a href="http://en.wikipedia.org/wiki/Quiz" target="_blank">What is a Quiz?</a></p>
+		<b><a style="text-decoration: none" href="https://www.linkedin.com/in/jon-arzelus-rodriguez-63306b128">Jon Arzelus</a> eta <a style="text-decoration: none" href='inaki.html'>Iñaki Berriotxoa</a></b>
 		<a href='https://github.com/berrio86/wsGit16'><img style="width:3%" src="irudiak/github-icon.png"></a>
+		<a href='http://en.wikipedia.org/wiki/Quiz'><img style="width:3%" src="irudiak/wikipedia-icon.png"></a>
 	</footer>
 </div>
 </body>
