13d5edf170993a38aacd83a0bd5b582e4c61b926
Inaki Berriotxoa ubuntu <berriotxoa86@gmail.com>
2016-11-04 12:17:37 +0100

    Derrigorrezko 2 ya bukatuta

--- php/nuSOAPbezPasahitza.php
+++ php/nuSOAPbezPasahitza.php
@@ -1,18 +1,20 @@
 <?php
-if (isset($_GET['eposta'])){
+if (isset($_GET['pasahitza'])){
 //nusoap.php klasea gehitzen dugu
 require_once('lib/nusoap.php');
 require_once('lib/class.wsdlcache.php');
 
 //soapclient mota dun objektua sortzen dugu. http://www.mydomain.com/server.php  erabiliko den SOAP zerbitzua non dagoen zehazten url horrek
-$soapclient = new nusoap_client("http://wsjiparsar.esy.es/webZerbitzuak/egiaztatuMatrikula.php?wsdl", false);
+$soapclient = new nusoap_client("http://localhost/myquizz/php/egiaztatuPasahitza.php?wsdl", true);
 
 //Web-Service-n inplementatu dugun funtzioari dei egiten diogu eta itzultzen diguna inprimatzen dugu
-$emaitza= $soapclient->call('pasahitzaKonprobatu',array( 'x'=>$_GET['eposta']));
-	if($emaitza == 'BAI'){
-		echo "Pasahitz hori egokia da";
+$emaitza= $soapclient->call('pasahitzaKonprobatu',array( 'x'=>$_GET['pasahitza']));
+
+	if(strcmp($emaitza,'BALIOZKOA')==0){
+		echo "Pasahitz hori egokia da".$emaitza."<br>";
 	}else{
-		echo "Pasahitz hori ohizkoa da, aldatu mezedez";
+		echo "Pasahitz hori ohizkoa da, aldatu mezedez".$emaitza."<br>";
+
 	}
 }
 ?>
