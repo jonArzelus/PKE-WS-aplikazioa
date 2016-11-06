4fcd2cd0034472d5d14183e4ad9c895b97b9c3b9
Inaki Berriotxoa ubuntu <berriotxoa86@gmail.com>
2016-11-06 21:04:16 +0100

    derrigorrezkoak eginda eta hautazko 2 eginda, arazoak zerbitzariarekin

--- php/egiaztatuPasahitza.php
+++ php/egiaztatuPasahitza.php
@@ -5,7 +5,7 @@
 require_once('lib/nusoap.php');
 require_once('lib/class.wsdlcache.php');
 
-$ns = "http://localhost/myquizz/php/egiaztatuPasahitza.php"; 
+$ns="http://localhost/myquizz/php/egiaztatuPasahitza.php"; 
 
 //soap_server motako objektua sortzen dugu
 $server = new soap_server;
@@ -14,21 +14,25 @@ $server->wsdl->schemaTargetNamespace=$ns;
 
 //inplementatu nahi dugun funtzioa erregistratzen dugu, funtzio bat baino gehiago erregistra liteke ...
 $server->register('pasahitzaKonprobatu',
-				  array('pass'=>'xsd:string'),
-				  array('z'=>'xsd:string'),
+				  array('pass'=>"xsd:string",'kodea'=>"xsd:int"),
+				  array('z'=>"xsd:string"),
 				  $ns);
 
 //funtzioa inplementatzen da
-function pasahitzaKonprobatu($pass){
-	if(strpos(file_get_contents("toppasswords.txt"),$pass)!==false){
-		return 'BALIOGABEA';
+function pasahitzaKonprobatu($pass,$kodea){
+	if(strpos(file_get_contents("kodeak.txt"),$kodea)!==false){
+		return 'BAIMENIK GABEKO ERABILTZAILEA';
 	}else{
-		return 'BALIOZKOA';
+		if(strpos(file_get_contents("toppasswords.txt"),$pass)!==false){
+			return 'BALIOGABEA';
+		}else{
+			return 'BALIOZKOA';
+		}
 	}
 }
 
 //nusoap klaseko service metodoari dei egiten diogu
 $HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : '';
 $server->service($HTTP_RAW_POST_DATA);
-//exit();
+
 ?>
--- php/nuSOAPbezPasahitza.php
+++ php/nuSOAPbezPasahitza.php
@@ -1,14 +1,16 @@
 <?php
-if (isset($_GET['pasahitza'])){
+if (isset($_GET['pasahitza'])&&isset($_GET['kodea'])){
 $pass=$_GET['pasahitza'];
+$code=$_GET['kodea'];
+
+
 //nusoap.php klasea gehitzen dugu
 //echo $pass;
 require_once('lib/nusoap.php');
 require_once('lib/class.wsdlcache.php');
 
 //soapclient mota dun objektua sortzen dugu. http://www.mydomain.com/server.php  erabiliko den SOAP zerbitzua non dagoen zehazten url horrek
-
-	//$soapclient = new nusoap_client("http://localhost/myquizz/php/egiaztatuPasahitza.php?wsdl", true);
+//$soapclient = new nusoap_client("http://localhost/myquizz/php/egiaztatuPasahitza.php?wsdl", true);
 $soapclient = new nusoap_client('http://igortxete.hol.es/myquiz/egiaztatuPasahitzaZerbitzari.php?wsdl', true);
 	
 /*$err=$soapclient->getError();
@@ -19,13 +21,22 @@ if($err){
 }*/
 	
 //Web-Service-n inplementatu dugun funtzioari dei egiten diogu eta itzultzen diguna inprimatzen dugu
-//$emaitza= $soapclient->call('pasahitzaKonprobatu',array('pass'=>$_GET['pasahitza']));
-$emaitza= $soapclient->call('validatePass',array('pass'=>$_GET['pasahitza']));
-//echo $emaitza;
+//$emaitza= $soapclient->call('pasahitzaKonprobatu',array('pass'=>"$pass",'code'=>"$code"));
+$emaitza= $soapclient->call('validatePass',array('pass'=>"$pass",'code'=>"$code"));
+
+	if(strcmp($emaitza,'BAIMENIK GABEKO ERABILTZAILEA')==0){
+		echo "Kode zenbakia ez da zuzena, sartu baliozko bat mesedez";
+	}
 	if(strcmp($emaitza,'BALIOZKOA')==0){
 		echo "Pasahitz hori egokia da";
-	}else{
+	}
+	if(strcmp($emaitza,'BALIOGABEA')==0){
 		echo "Pasahitz hori ohizkoa da, aldatu mezedez";
 	}
+	
+/*echo '<h2>Request</h2><pre>'.htmlspecialchars($soapclient->request, ENT_QUOTES).'</pre>';
+echo '<h2>Response</h2><pre>'.htmlspecialchars($soapclient->response, ENT_QUOTES).'</pre>';
+echo '<h2> Debug </h2>';
+echo '<pre>'.htmlspecialchars($soapclient->debug_str, ENT_QUOTES).'</pre>';*/
 }
 ?>
