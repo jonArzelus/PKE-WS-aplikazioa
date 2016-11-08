f8fd256f361cbeeaf3da72833e717519b052270b
jonArzelus <kontrabass32@hotmail.com>
2016-11-08 16:13:49 +0100

    irudia aldatua credits-en

--- credits.php
+++ credits.php
@@ -10,7 +10,7 @@
 	<div>
 		<h2><a style="text-decoration: none" href="https://www.linkedin.com/in/jon-arzelus-rodriguez-63306b128">Jon Arzelus</a> eta <a style="text-decoration: none" href='inaki.html'>Iñaki Berriotxoa</a></h2><br>
 		<h3>Software Ingeniaritza espezialitatea</h3><br>
-		<img src="irudiak/index.jpeg" alt="computer_software"><br>
+		<img src="irudiak/index.gif" alt="computer_software"><br>
 		Web Sistemak ikasgaieko lehen laborategiko lana da hau, pixkanaka osatzen joango gara
 	</div>
 	<div id="soap_geo">
